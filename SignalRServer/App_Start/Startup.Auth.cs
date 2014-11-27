e3eb052c77f3161187207585baf7543bc90610d7
ITSPaul <powell.paul@itsligo.ie>
2014-11-27 00:40:14 +0000

    Facebook registration authentication and wall posting implemented

---
+++ SignalRServer/App_Start/Startup.Auth.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Google;
+using Owin;
+using SignalRServer.Models;
+using Microsoft.Owin.Security.Facebook;
+
+namespace SignalRServer
+{
+    public partial class Startup
+    {
+        // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.MapSignalR();
+            // Configure the db context, user manager and signin manager to use a single instance per request
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
+
+            // Enable the application to use a cookie to store information for the signed in user
+            // and to use a cookie to temporarily store information about a user logging in with a third party login provider
+            // Configure the sign in cookie
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+                Provider = new CookieAuthenticationProvider
+                {
+                    // Enables the application to validate the security stamp when the user logs in.
+                    // This is a security feature which is used when you change a password or add an external login to your account.  
+                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
+                        validateInterval: TimeSpan.FromMinutes(30),
+                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
+                }
+            });            
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
+            app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
+
+            // Enables the application to remember the second login verification factor such as phone or email.
+            // Once you check this option, your second step of verification during the login process will be remembered on the device where you logged in from.
+            // This is similar to the RememberMe option when you log in.
+            app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
+
+            // Uncomment the following lines to enable logging in with third party login providers
+            //app.UseMicrosoftAccountAuthentication(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            //app.UseTwitterAuthentication(
+            //   consumerKey: "",
+            //   consumerSecret: "");
+
+            var x = new FacebookAuthenticationOptions();
+            x.Scope.Add("email");
+            x.AppId = "328961853957950";
+            x.AppSecret = "2a9461935ee1229dc321ce6a47dd4fdb";
+            x.Scope.Add("publish_actions");
+            x.Provider = new FacebookAuthenticationProvider()
+            {
+                OnAuthenticated = async context =>
+                {
+                    var rawUserObjectFromFacebookAsJson = context.User;
+
+                    context.Identity.AddClaim(
+                        new System.Security.Claims.Claim("FacebookAccessToken", context.AccessToken));
+                    foreach (var claim in context.User)
+                    {
+                        var claimType = string.Format("urn:facebook:{0}", claim.Key);
+                        string claimValue = claim.Value.ToString();
+                        if (!context.Identity.HasClaim(claimType, claimValue))
+                            context.Identity.AddClaim(new System.Security.Claims.Claim(claimType, claimValue, "XmlSchemaString", "Facebook"));
+                    }
+
+                }
+            };
+
+            x.SignInAsAuthenticationType = DefaultAuthenticationTypes.ExternalCookie;
+            app.UseFacebookAuthentication(x);
+
+            //app.UseFacebookAuthentication(
+            //   appId: "328961853957950",
+            //   appSecret: "2a9461935ee1229dc321ce6a47dd4fdb");
+
+            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            //{
+            //    ClientId = "",
+            //    ClientSecret = ""
+            //});
+        }
+    }
+}
---
+++ SignalRServer/GameR.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRServer
+{
+    public enum COLLECTABLE_TYPE { STANDARD, MEDIUM, COMPLEX}
+
+    public class Player
+    {
+        int _playerID;
+
+        public int PlayerID
+        {
+            get { return _playerID; }
+            set { _playerID = value; }
+        }
+        Vector2 _position;
+
+        public Vector2 Position
+        {
+            get { return _position; }
+            set { _position = value; }
+        }
+
+        int _score;
+
+        public int Score
+        {
+            get { return _score; }
+            set { _score = value; }
+        }
+
+        public bool _firstIn;
+
+        public Player(int id, Vector2 pos)
+        {
+            _playerID = id;
+            _position = pos;
+        }
+    }
+
+    public enum GAMESTATE { NONE, STARTING, PLAYING, ENDING, FINISHED }
+
+    public class Collectable
+    {
+        int _id;
+        COLLECTABLE_TYPE _type;
+
+        public COLLECTABLE_TYPE Type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        int _value;
+        bool _collected;
+
+        public bool Collected
+        {
+            get { return _collected; }
+            set { _collected = value; }
+        }
+        public int Value
+        {
+            get { return _value; }
+            set { _value = value; }
+        }
+        Vector2 _position;
+        public Vector2 Position
+        {
+            get { return _position; }
+            set { _position = value; }
+        }
+        
+        // colleectale value should be 10, 20 or 30
+        public Collectable(int id, Vector2 pos, int value)
+        {
+            _id = id;
+            _position = pos;
+            _value = value;
+            _collected = false;
+            switch (value)
+            {
+                case 10:
+                    _type = COLLECTABLE_TYPE.STANDARD;
+                    break;
+                case 20:
+                    _type = COLLECTABLE_TYPE.MEDIUM;
+                    break;
+                case 30:
+                    _type = COLLECTABLE_TYPE.COMPLEX;
+                    break;
+            }
+        }
+    }
+}
